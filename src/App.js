321d58df974ba40104be7410b52abfc476fbc436
leemhoon00 <leemhoon000@gmail.com>
2023-10-28 17:23:59 +0900

    use callback function

--- src/App.js
+++ src/App.js
@@ -3,9 +3,22 @@ import DetectCrawlingReact from "detect-crawling-react";
 import Router from "./Router.js";
 
 function App() {
+  const callback = async () => {
+    const response = await fetch("http://3.36.31.6:8000/react/", {
+      method: "GET",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    if (response.status === 200) {
+      alert("crawling detected");
+      window.location.href =
+        "http://crawling-ALB-37796986.ap-northeast-2.elb.amazonaws.com";
+    }
+  };
   return (
     <div className="App">
-      <DetectCrawlingReact backURL={"http://3.36.31.6:8000/react/"} />
+      <DetectCrawlingReact callback={callback} />
       <Router />
     </div>
   );
